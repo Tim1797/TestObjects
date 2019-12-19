09a139ad38751e33f84ce054836f3074401dfbd1
Tim1797 <53017505+Tim1797@users.noreply.github.com>
2019-12-19 15:35:55 +0100

    Add files via upload

---
+++ src/translator/Addition.java
@@ -0,0 +1,19 @@
+package translator;
+
+/**
+  * This class represents an Addition within the Java language.
+  * This is represented in the Java source code by the '+' character.
+  *
+  * @author  Brian Folts
+  * @since   1.0
+  */
+public class Addition {
+
+	/**
+	 * Default addition constructor.
+	 */
+	public Addition() {
+	
+	}
+
+}
