d3026602111cfdd58a86da6592095a68fe397b15
Tim1797 <53017505+Tim1797@users.noreply.github.com>
2020-03-26 14:33:58 +0100

    Add files via upload

---
+++ presentation/ModifiedClass.java
@@ -0,0 +1,17 @@
+package examplePackage;
+
+public class ModifiedClass {
+
+	static int fib(int n) { 
+		if (n <= 2) {
+			return n; 
+		}			
+		return fib(n-1) + fib(n-2); 
+    } 
+       
+    public static void main (String args[]) { 
+    	int n = 10; 
+    	System.out.println(fib(n)); 
+    } 
+    
+}
